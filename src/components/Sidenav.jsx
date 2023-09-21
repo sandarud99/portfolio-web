8b9f0e28d20673c22fb5e1b8510e72d7e36dfe53
sandarud99 <2020cs046@stu.ucsc.cmb.ac.lk>
2023-09-21 11:46:53 +0530

    Fixed color of menu icon and fixed download issues

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -30,7 +30,7 @@ const Sidenav = () => {
     <div>
       <AiOutlineMenu
         onClick={handleNav}
-        className="absolute top-4 right-4 z-[99] md:hidden"
+        className="absolute top-4 right-4 z-[99] md:hidden text-blue-400"
       />
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
@@ -60,7 +60,7 @@ const Sidenav = () => {
             <span className="pl-4">Projects</span>
           </a>
           <a
-            onClick={handleNav}
+            onClick={downloadResume}
             href="#resume"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 "
           >
